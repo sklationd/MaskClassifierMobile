3350de10f615287fc78b25a83d3ede3e631c6014
sklationd <sklationd@gmail.com>
2020-11-23 10:18:09 +0900

    add: new model
    It is not satisfied, but it has improved a lot.

--- app/src/main/assets/scripts.py
+++ app/src/main/assets/scripts.py
@@ -1,3 +1,6 @@
 import torch
-torchmodel = torch.jit.load("best.pt", map_location='cpu')
-torch.jit.save(torchmodel, "best_cpu.pt")
+quantized_torchmodel = torch.jit.load("quantized_best.pt", map_location='cpu')
+torch.jit.save(quantized_torchmodel, "best_cpu_quantized.pt")
+
+scripted_torchmodel = torch.jit.load("scripted_best.pt", map_location='cpu')
+torch.jit.save(scripted_torchmodel, "best_cpu_scripted.pt")
--- app/src/main/java/org/pytorch/demo/MainActivity.java
+++ app/src/main/java/org/pytorch/demo/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         v -> {
           final Intent intent = new Intent(MainActivity.this, ImageClassificationActivity.class);
           intent.putExtra(ImageClassificationActivity.INTENT_MODULE_ASSET_NAME,
-                  "best_cpu.pt");
+                  "best_cpu_quantized.pt");
           intent.putExtra(ImageClassificationActivity.INTENT_INFO_VIEW_TYPE,
                   InfoViewFactory.INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_QMOBILENET);
           startActivity(intent);
